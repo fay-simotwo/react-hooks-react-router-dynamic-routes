28f1b3666514f1c5499af8a9e903d156865e0bd1
Moringa School <chepfaith18@gmail.com>
2023-07-27 14:49:44 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,8 @@ function App() {
   const [movies, setMovies] = useState([
     { id: 1, title: "A River Runs Through It" },
     { id: 2, title: "Se7en" },
-    { id: 3, title: "Inception" }
-  ]);
+    { id: 3, title: "Inception" },
+    ]);
 
   return (
     <div>
